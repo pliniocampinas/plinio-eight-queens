bee3e17151495a47b9cc65b6a324e0cf552360ec
Plinioc <plinio.lsc@gmail.com>
2023-10-15 19:50:08 -0300

    Implement print_table in order to display table to console

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 fn main() {
     println!("Eight Queens");
 
-    let table: [u8; 64] = [1; 64];
+    let table: [u8; 64] = [0; 64];
 
     let [ line, col ] = [ 4 , 6 ];
     let cell_value = get_table_value(&table, line, col);
 
     println!("Table line {}, col: {}", line, col);
-    println!("Table {}", table[63]);
-    assert!(cell_value == 1);
+    print_table(&table);
+    assert!(cell_value == 0);
 }
 
 fn get_table_value(&table: &[u8; 64], line: usize, col: usize) -> u8 {
     let index = (line * col) - 1;
     return *table.get(index).unwrap();
+}
+
+fn print_table(&table: &[u8; 64]) {
+    println!("-------------- Table ------------- ");
+    let mut index = 0;
+    for elem in table {
+        if index % 8 == 0 {
+            println!();
+            print!("|");
+        }
+        index+=1;
+        print!(" {} ", elem);
+        print!("|");
+    }
+    println!();
+    println!("----------- ---------- ---------- ");
 }
