ef767c700ce6d33389b1fc87557754182ee737a8
Plinioc <plinio.lsc@gmail.com>
2023-10-15 21:09:51 -0300

    Print table with details

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
     println!("Eight Queens");
 
-    let table: [u8; 64] = [0; 64];
+    let mut table: [u8; 64] = [0; 64];
 
     let [ line, col ] = [ 4 , 6 ];
+    set_table_value(&mut table, line, col, 1);
     let cell_value = get_table_value(&table, line, col);
 
     println!("Table line {}, col: {}", line, col);
     print_table(&table);
-    assert!(cell_value == 0);
+    assert!(cell_value == 1);
 }
 
 fn get_table_value(&table: &[u8; 64], line: usize, col: usize) -> u8 {
@@ -16,18 +17,30 @@ fn get_table_value(&table: &[u8; 64], line: usize, col: usize) -> u8 {
     return *table.get(index).unwrap();
 }
 
+fn set_table_value(table: & mut [u8; 64], line: usize, col: usize, value: u8) {
+    let index = (line * col) - 1;
+    table[index] = value;
+}
+
 fn print_table(&table: &[u8; 64]) {
-    println!("-------------- Table ------------- ");
+    println!("-------------- Table -------------- ");
+    print!("x ");
+    for i in 1..9 {
+        print!("| {} ", i);
+    }
+    println!("|");
+    print!("-------------- ----- -------------- ");
+
     let mut index = 0;
     for elem in table {
         if index % 8 == 0 {
             println!();
-            print!("|");
+            print!("{} |", (index/8 + 1));
         }
         index+=1;
         print!(" {} ", elem);
         print!("|");
     }
     println!();
-    println!("----------- ---------- ---------- ");
+    println!("----------- ---------- ------------ ");
 }
